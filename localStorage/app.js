3caf7b0e78cbffeda277acf0190269aa40e0b0d7
Jasurbek <99987586+yoljasro@users.noreply.github.com>
2022-09-27 10:59:30 +0500

    Add files via upload

---
+++ localStorage/app.js
@@ -0,0 +1,27 @@
+// localStorage.setItem("lastname", "Jasur");
+// // Retrieve
+// document.getElementById("demo").innerHTML = localStorage.setItem("lastname");
+
+// function populateStorage() {
+//     localStorage.setItem('bgcolor', 'red');
+//     localStorage.setItem('font', 'Helvetica');
+//     localStorage.setItem('image', 'myCat.png');
+
+//     document.getElementById('demo').innerHTML = localStorage.getItem("font")
+//     localStorage.removeItem("bgcolor")
+//   }
+// localStorage.clear()
+
+
+// function store(){
+//   var inputEmail= document.getElementById("email");
+//   localStorage.setItem("email", inputEmail.value);
+//  }
+
+
+function populateStorage() {
+    localStorage.setItem('bgcolor', 'red');
+    localStorage.setItem('font', 'Helvetica');
+    localStorage.setItem('image', 'myCat.png');
+  }
+ 
